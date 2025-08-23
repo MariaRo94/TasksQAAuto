e54bbeb9b120ba3019213d9fbde4b6b16f18b395
Maria Rodionova <mrodionova@aflt-systems.ru>
2025-08-23 19:04:14 +0300

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,86 @@
+package org.example;
+
+public class Animal {
+    private String name;
+    private int age;
+    private long weight;
+    private String color;
+
+    public Animal(String name, int age, long weight, String color) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public void say() {
+        System.out.println("Я говорю");
+    }
+
+    public void go(){
+        System.out.println("Я иду");
+    }
+
+    public void drink(){
+        System.out.println("Я пью");
+    }
+
+    public void eat (){
+        System.out.println("Я ем");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(long weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAgeContext(int age) {
+        int lastDigit = age % 10;
+        int lastTwoDigits = age % 100;
+
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+            return " лет";
+        }
+        switch (lastDigit) {
+            case 1:
+                return " год";
+            case 2, 3, 4:
+                return " года";
+            default:
+                return " лет";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ", мне " + age + getAgeContext(age)
+                + " , я вешу - " + weight + "кг, мой цвет - " + color;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal jassy = new Animal("Jasonita", 2, 4, "black");
+        System.out.println(jassy);
+    }
+}
